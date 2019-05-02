1353d56253b1564e33ff3a10dedc61420e087310
Air <angelo@vadiks-air.speedport.ip>
2019-05-02 19:56:07 +0200

    VE comment

---
+++ Main.java
@@ -0,0 +1,15 @@
+package HelloWorld;
+
+# Comment from Vlad
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        boolean flag = true;
+        System.out.println("Hello world");
+        System.out.println("Hello from Andrew");
+        System.out.println("Hello again");
+        while(flag != false) {
+            System.out.println("I'm alive!");
+            Thread.sleep(1000);
+        }
+    }
+}
